606ed54d10273fc1615bc15f38e13a695fcf70a9
Dani55Alv <alvarezmdanielel62@gmai.com>
2025-03-26 11:35:42 +0100

    Solucinado_Error_raro

--- Ficheros_Java/EjercicioFicheroBloque2/EjFicheroBloque2/src/gestionFicherosBloque2.java
+++ Ficheros_Java/EjercicioFicheroBloque2/EjFicheroBloque2/src/gestionFicherosBloque2.java
@@ -6,18 +6,14 @@ import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
-public class gestionFicheros {
+public class gestionFicherosBloque2 {
 
     // C:\Users\daniy\OneDrive\Escritorio\visualStudioClases\gitHub\RepositorioClase\Alvarez_Daniel_PRO_2425\Ficheros_Java\EjercicioFicheroBloque1\ejFicheroBloque1
 private String nombreGestion;
-    public gestionFicheros(String nombreGestion){
+    public gestionFicherosBloque2(String nombreGestion){
         this.nombreGestion = nombreGestion;
     }
-    public void textoParaVocalesSeparadoConsonante(String ruta, String archivo) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'textoParaVocalesSeparadoConsonante'");
-    }
-
+ 
 
 
 
