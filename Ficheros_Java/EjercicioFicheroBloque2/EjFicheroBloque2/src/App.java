eae6dce9038647e6c156f25363c916aee2f10620
Dani55Alv <alvarezmdanielel62@gmai.com>
2025-03-26 12:27:25 +0100

    Bloque2Ejercicio2

--- Ficheros_Java/EjercicioFicheroBloque2/EjFicheroBloque2/src/App.java
+++ Ficheros_Java/EjercicioFicheroBloque2/EjFicheroBloque2/src/App.java
@@ -1,8 +1,35 @@
+import java.util.Scanner;
 
 public class App {
 public static void ejercicio2(){
 
+    
+    System.out.println("Vamos a combinar un archivo");
+ 
+    gestionFicherosBloque2 gestionFicherosBloque2 = new gestionFicherosBloque2("Gestion2");
 
+    Scanner sc = new Scanner(System.in);
+    
+    String ruta = "";
+    String nombreFichero= "";
+    String a = "";
+    String b = "";
+
+    System.out.println("Introduce la ruta comun");
+         ruta=sc.nextLine();
+    System.out.println("Introduce el nombre del fichero combinado");
+       nombreFichero= sc.nextLine();
+
+       System.out.println("Introduce el nombre del fichero a combinar (A)");
+       a = sc.nextLine();
+
+       System.out.println("Introduce el nombre del fichero a combinar (B)");
+       b = sc.nextLine();
+
+       gestionFicherosBloque2.combinarArchivo(nombreFichero, a, b, ruta);
+
+
+       
 }
     public static void main(String[] args) throws Exception {
 
--- Ficheros_Java/EjercicioFicheroBloque2/EjFicheroBloque2/src/gestionFicherosBloque2.java
+++ Ficheros_Java/EjercicioFicheroBloque2/EjFicheroBloque2/src/gestionFicherosBloque2.java
@@ -12,10 +12,84 @@ public class gestionFicherosBloque2 {
 private String nombreGestion;
     public gestionFicherosBloque2(String nombreGestion){
         this.nombreGestion = nombreGestion;
+
+    }
+
+    public void combinarArchivo(String archivoCombinar, String archivoA, String archivoB, String rutaComun){
+ 
+    Path ruta = Paths.get(rutaComun, archivoCombinar);
+
+        if (Files.exists(ruta)) {
+      List<String> b =    leerFichero1(rutaComun, archivoB);
+
+      List<String> a = leerFichero1(rutaComun, archivoA);
+
+      escribirFichero(rutaComun, archivoCombinar, b, a );
+        } else {
+            // List string
+            System.out.println("No existe el fichero a combinar");
+        }
+
+   
     }
  
+    public void escribirFichero(String ruta, String nombreFichero, List<String> contenidoPorLineaA, 
+            List<String> contenidoPorLineaB) {
+
+              Path rutaDef = Paths.get(ruta, nombreFichero);
+
+        try {
+            if (Files.exists(rutaDef)) {
+                int maxLength = Math.max(contenidoPorLineaA.size(), contenidoPorLineaB.size());
+                for (int i = 0; i < maxLength; i++) {
+                    // Si la lista A tiene una línea en la posición i, escribirla
+                    if (i < contenidoPorLineaA.size()) {
+                        Files.write(rutaDef, (contenidoPorLineaA.get(i) + System.lineSeparator()).getBytes(),
+                                StandardOpenOption.APPEND);
+                    }
+                    // Si la lista B tiene una línea en la posición i, escribirla
+                    if (i < contenidoPorLineaB.size()) {
+                        Files.write(rutaDef, (contenidoPorLineaB.get(i) + System.lineSeparator()).getBytes(),
+                                StandardOpenOption.APPEND);
+                    }
+                }
+            } else {
+                // List string
+                System.out.println("No existe el fichero a escribir");
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
+    public List<String> leerFichero1(String ruta, String archivo) {
 
-    
+        Path rutaDef = Paths.get(ruta, archivo);
+
+        List<String> archivoTexto;
+        List<String> textoTotal = new ArrayList<>();
+
+        try {
+            if (Files.notExists(rutaDef)) {
+                System.out.println("El archivo no existe");
+            } else {
+
+                archivoTexto = Files.readAllLines(rutaDef);
+                System.out.println("Reproduciendo por linea el texto");
+                for (String linea : archivoTexto) {
+                    System.out.println(linea);
+                        textoTotal.add(linea);
+                    }     
+                    System.out.println("Fin");
+            }
+
+        } catch (Exception e) {
+            System.err.println("Error");
+            e.printStackTrace();
+        }
+return textoTotal;
+    }
 }
