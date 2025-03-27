2052df1a98f2f1eae85acac38b5663b2575d137e
Dani55Alv <alvarezmdanielel62@gmai.com>
2025-03-27 12:38:20 +0100

    Censurando_con_lista_palabras

--- Ficheros_Java/EjercicioCharToxico/chatToxico/src/Main.java
+++ Ficheros_Java/EjercicioCharToxico/chatToxico/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -20,6 +21,13 @@ public class Main {
         String palabraPorCaractar = "";
         String espantapájaros = "espantapájaros";
 
+        
+        // Lista de palabras a censurar
+        List<String> palabrasCensuradas = new ArrayList<>();
+        palabrasCensuradas.add("espantapájaros");
+        palabrasCensuradas.add("pelotudo");
+        palabrasCensuradas.add("manco");
+
         for (String linea : chat) {
             lineaPorCaractar = ""; // Limpiamos la línea por carácter al empezar con una nueva línea
             for (int i = 0; i < linea.length(); i++) {
@@ -28,8 +36,9 @@ public class Main {
                 lineaPorCaractar += caracter; // Imprime las líneas char a char
 
                 // Si encontramos la palabra "espantapájaros", la censuramos
-                if (palabraPorCaractar.equals("espantapájaros")) {
-                    System.out.println("Palabra mal sonante detectada");
+               for (String palabra : palabrasCensuradas) {
+                    if (palabraPorCaractar.equals(palabra)) {
+                        System.out.println("Palabra mal sonante detectada: " + palabra);
 
                     // Reemplazamos con asteriscos
                     lineaPorCaractar = lineaPorCaractar.substring(0,
@@ -45,6 +54,7 @@ public class Main {
                     // Reiniciamos la variable para seguir buscando otras palabras
                     palabraPorCaractar = "";
                 }
+            }
 
                 // Si encontramos un espacio, punto o coma, consideramos que hemos llegado al
                 // final de una palabra
